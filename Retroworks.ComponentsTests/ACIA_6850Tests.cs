43025e9865996b034a44cecb2fd7adaf07a63852
Carl Christerson <carlcnx@outlook.com>
2025-05-24 21:33:16 +0900

    Add project Retroworks.Components.Tests

---
+++ Retroworks.ComponentsTests/ACIA_6850Tests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Retroworks.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retroworks.Components.Tests
+{
+    [TestClass()]
+    public class ACIA_6850Tests
+    {
+        [TestMethod()]
+        public void ReadPortTest()
+        {
+        }
+    }
+}
